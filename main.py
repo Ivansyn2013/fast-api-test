3435089626ff887bf422983d12a55a540b4bd058
ivansyn2013 <ivansyn2013@yandex.ru>
2022-03-20 19:28:11 +0300

    create some files

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import uvicorn
 from fastapi import FastAPI
+from db.base import database
+import uvicorn
 
-
-app= FastAPI()
+app = FastAPI()
 
 
 @app.get('/')
 async def root():
     return {'message': 'Hellow World'}
 
+
+@app.on_event('startup')
+async def startup():
+    await database.connect()
+
+@app.on_event('shutdwn')
+async def shutdown():
+    await database.disconnect()
+
+
 if __name__ == '__main__':
-    uvicorn.run('main:app', port=8000, host='0.0.0.0', reload = True)
+    uvicorn.run('main:app', port=8000, host='0.0.0.0', reload=True)
